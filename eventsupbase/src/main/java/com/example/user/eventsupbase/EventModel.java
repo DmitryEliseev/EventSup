4df2261d1f002158314a7a1ea1e3f0c6da7ccaba
DmitryEliseev <dima-eliseev10@rambler.ru>
2016-04-21 14:19:00 +0300

    Adding DataModel files

---
+++ eventsupbase/src/main/java/com/example/user/eventsupbase/EventModel.java
@@ -0,0 +1,12 @@
+package com.example.user.eventsupbase;
+
+import java.util.List;
+
+public class EventModel {
+    String id_event;
+    String event_name;
+    String date_start;
+    String date_finish;
+    String picture;
+    List<ReportModel> reports;
+}
---
+++ eventsupbase/src/main/java/com/example/user/eventsupbase/ReportModel.java
@@ -0,0 +1,13 @@
+package com.example.user.eventsupbase;
+
+import java.util.List;
+
+public class ReportModel {
+    String id_report;
+    String report_name;
+    String time;
+    String address;
+    String lecture_hall;
+    String description;
+    List<String> authors;
+}
